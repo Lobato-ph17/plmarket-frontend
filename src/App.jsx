a46fbdbbc7a7e22387a7331d228c88e041bdda24
Pedro Lobato <lobato.ph17@gmail.com>
2026-07-08 20:34:42 -0300

    feat: inicializa plmarket-frontend com react, vite e eslint

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ background: '#121212', color: '#fff', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontFamily: 'sans-serif' }}>
+      <h1>plmarket - Frontend Inicializado</h1>
+    </div>
+  );
+}
+
+export default App;
